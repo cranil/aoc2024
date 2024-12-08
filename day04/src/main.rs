01716765a6d9aac1de47748174476baabfc91175
Anil Raghuramu <contact@cranil.io>
2024-12-08 15:17:18 +0000

    day04: change some conditions to make it more efficient

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,176 +1,141 @@
-fn solve1(input: &str) -> i64 {
-    let lines = input.lines().collect::<Vec<&str>>();
-    let mut grid = vec![vec![' '; lines[0].len()]; lines.len()];
-    for (i, line) in lines.iter().enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            grid[i][j] = c;
-        }
-    }
+fn solve1(grid: &[Vec<char>]) -> i64 {
     let mut count = 0;
     const XMAS: &str = "XMAS";
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
             if grid[i][j] == 'X' {
                 // horizontal
-                let mut found = true;
-                for k in 0..4 {
-                    if j + k >= grid[0].len() {
-                        found = false;
-                        break;
+                if j + 3 < grid[0].len() {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i][j + k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i][j + k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+                    if found {
+                        count += 1;
                     }
-                }
-                if found {
-                    count += 1;
                 }
 
-                found = true;
                 // horizontal reverse
-                for k in 0..4 {
-                    if (j as i32) - (k as i32) < 0 {
-                        found = false;
-                        break;
+                if j >= 3 {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i][j - k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i][j - k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+                    if found {
+                        count += 1;
                     }
                 }
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // vertical
-                for k in 0..4 {
-                    if i + k >= grid.len() {
-                        found = false;
-                        break;
+                if i + 3 < grid.len() {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i + k][j] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i + k][j] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+
+                    if found {
+                        count += 1;
                     }
                 }
-
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // vertical reverse
-
-                for k in 0..4 {
-                    if (i as i32) - (k as i32) < 0 {
-                        found = false;
-                        break;
+                if i >= 3 {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i - k][j] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i - k][j] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+
+                    if found {
+                        count += 1;
                     }
                 }
-
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // diagonal
-                for k in 0..4 {
-                    if i + k >= grid.len() || j + k >= grid[0].len() {
-                        found = false;
-                        break;
+                if i + 3 < grid.len() && j + 3 < grid[0].len() {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i + k][j + k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i + k][j + k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+
+                    if found {
+                        count += 1;
                     }
                 }
-
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // diagonal reverse
-
-                for k in 0..4 {
-                    if (i as i32) - (k as i32) < 0
-                        || (j as i32) - (k as i32) < 0
-                    {
-                        found = false;
-                        break;
+                if i >= 3 && j >= 3 {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i - k][j - k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i - k][j - k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+
+                    if found {
+                        count += 1;
                     }
                 }
-
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // off-diagonal
-
-                for k in 0..4 {
-                    if (i as i32) - (k as i32) < 0 || j + k >= grid[0].len() {
-                        found = false;
-                        break;
+                if i >= 3 && j + 3 < grid[0].len() {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i - k][j + k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i - k][j + k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
+
+                    if found {
+                        count += 1;
                     }
                 }
-
-                if found {
-                    count += 1;
-                }
-
-                found = true;
 
                 // off-diagonal reverse
-
-                for k in 0..4 {
-                    if i + k >= grid.len() || (j as i32) - (k as i32) < 0 {
-                        found = false;
-                        break;
+                if i + 3 < grid.len() && j >= 3 {
+                    let mut found = true;
+                    for k in 0..4 {
+                        if grid[i + k][j - k] == XMAS.chars().nth(k).unwrap() {
+                            continue;
+                        } else {
+                            found = false;
+                            break;
+                        }
                     }
-                    if grid[i + k][j - k] == XMAS.chars().nth(k).unwrap() {
-                        continue;
-                    } else {
-                        found = false;
-                        break;
-                    }
-                }
 
-                if found {
-                    count += 1;
+                    if found {
+                        count += 1;
+                    }
                 }
             }
         }
@@ -178,16 +143,8 @@ fn solve1(input: &str) -> i64 {
     count
 }
 
-fn solve2(input: &str) -> i64 {
+fn solve2(grid: &[Vec<char>]) -> i64 {
     let mut count = 0;
-    let lines = input.lines().collect::<Vec<&str>>();
-    let mut grid = vec![vec![' '; lines[0].len()]; lines.len()];
-    for (i, line) in lines.iter().enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            grid[i][j] = c;
-        }
-    }
-
     for i in 1..grid.len() - 1 {
         for j in 1..grid[0].len() - 1 {
             if grid[i][j] == 'A' {
@@ -200,6 +157,7 @@ fn solve2(input: &str) -> i64 {
                 cond = cond && grid[i + 1][j - 1] == 'S';
                 if cond {
                     count += 1;
+                    continue;
                 }
 
                 // S.S
@@ -211,6 +169,7 @@ fn solve2(input: &str) -> i64 {
                 cond = cond && grid[i + 1][j - 1] == 'M';
                 if cond {
                     count += 1;
+                    continue;
                 }
 
                 // M.S
@@ -222,6 +181,7 @@ fn solve2(input: &str) -> i64 {
                 cond = cond && grid[i + 1][j - 1] == 'M';
                 if cond {
                     count += 1;
+                    continue;
                 }
 
                 // S.M
@@ -233,6 +193,7 @@ fn solve2(input: &str) -> i64 {
                 cond = cond && grid[i + 1][j - 1] == 'S';
                 if cond {
                     count += 1;
+                    continue;
                 }
             }
         }
@@ -242,6 +203,10 @@ fn solve2(input: &str) -> i64 {
 
 fn main() {
     let input = include_str!("../input.txt");
+    let grid = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
     let fs = [solve1, solve2];
-    solver::solver(fs, input);
+    solver::solver(fs, &grid);
 }
